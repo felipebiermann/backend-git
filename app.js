220d19294e0148acf5ef3bb77ade9c25ddd14059
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-07-12 16:21:00 -0300

    create done.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const userRouter = require("./Routes/user.routes");
+app.use("/user", userRouter);
+
 app.listen(4000, () => {
   console.log("Servidor rodando na porta 4000.");
 });
